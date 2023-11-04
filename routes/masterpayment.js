7a5cafafd769ad0362cdf77576e608fcb280196a
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-11-04 19:50:24 +0800

    Added status and edit api on masterpayment

--- routes/masterpayment.js
+++ routes/masterpayment.js
@@ -9,7 +9,7 @@ const {
 } = require("./repository/soisdb.js");
 
 const helper = require("./repository/customhelper.js");
-const disctionary = require("./repository/dictionary.js");
+const dictionary = require("./repository/dictionary.js");
 const { MasterPayment } = require("./model/soismodel.js");
 const { Validator } = require("./controller/middleware.js");
 
@@ -50,7 +50,7 @@ router.get("/load", (req, res) => {
 router.post("/save", (req, res) => {
   try {
     const { name } = req.body;
-    let status = disctionary.GetValue(disctionary.ACT());
+    let status = dictionary.GetValue(dictionary.ACT());
     let createdby =
       req.session.fullname == null ? "TESTER" : req.session.fullname;
     let createddate = helper.GetCurrentDatetime();
@@ -64,6 +64,67 @@ router.post("/save", (req, res) => {
       res.json({
         msg: "success",
       });
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
+
+router.post("/status", (req, res) => {
+  try {
+    const { id } = req.body;
+    let status =
+      req.body.status == dictionary.GetValue(dictionary.ACT())
+        ? dictionary.GetValue(dictionary.INACT())
+        : dictionary.GetValue(dictionary.ACT());
+    let data = [status, id];
+    console.log(data);
+
+    let sql = `update master_payment 
+                       set mp_status = ?
+                       where mp_id = ?`;
+
+    UpdateMultiple(sql, data, (err, result) => {
+      if (err) console.error("Error: ", err);
+      console.log(result);
+      res.json({
+        msg: "success",
+      });
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
+
+router.post("/edit", (req, res) => {
+  try {
+    const { payment, id } = req.body;
+
+    let data = [payment, id];
+    console.log(data);
+    let sql = `UPDATE master_payment 
+                       SET mp_name = ?
+                       WHERE mp_id = ?`;
+
+    UpdateMultiple(sql, data, (err, result) => {
+      if (err) console.error("Error: ", err);
+
+      console.log(result);
+
+      // let loglevel = dictionary.INF();
+      // let source = dictionary.MSTR();
+      // let message = `${dictionary.GetValue(dictionary.UPDT())} -  [${sql}]`;
+      // let user = req.session.employeeid;
+
+      // Logger(loglevel, source, message, user);
+    });
+
+    res.json({
+      msg: "success",
     });
   } catch (error) {
     res.json({
