eed4ff3a5459079631373de4b695f34adc552da4
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-11-05 17:42:35 +0800

    Added eduit API on masterstore

--- routes/masterstore.js
+++ routes/masterstore.js
@@ -47,14 +47,58 @@ router.get("/load", (req, res) => {
 
 router.post("/save", (req, res) => {
   try {
-    const { name, logo, address, contact, message } = req.body;
-    let master_store = [[name, logo, address, contact, message]];
+    const { name, logo, address, contact, messages } = req.body;
+    let master_store = [[name, logo, address, contact, messages]];
 
-    console.log(name, logo, address, contact, message);
+    console.log(name, logo, address, contact, messages);
 
     InsertTable("master_store", master_store, (err, result) => {
       if (err) console.error("Error: ", err);
 
+      console.log(result);
+
+      res.json({
+        msg: "success",
+      });
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+    });
+  }
+});
+
+router.post("/edit", (req, res) => {
+  try {
+    const { id, address, contact, messages } = req.body;
+    let data = [];
+    let sql_Update = `UPDATE master_store SET`;
+
+    console.log(`${id} ${address} ${contact} ${messages}`);
+
+    if (address) {
+      sql_Update += ` ms_address = ?,`;
+      data.push(address);
+    }
+
+    if (contact) {
+      sql_Update += ` ms_contact = ?,`;
+      data.push(contact);
+    }
+
+    if (messages) {
+      sql_Update += ` ms_message = ?,`;
+      data.push(messages);
+    }
+
+    sql_Update = sql_Update.slice(0, -1);
+    sql_Update += ` WHERE ms_id = ?;`;
+    data.push(id);
+
+    console.log(`${sql_Update} ${data}`);
+
+    UpdateMultiple(sql_Update, data, (err, result) => {
+      if (err) console.error("Error: ", err);
       console.log(result);
 
       res.json({
@@ -96,3 +140,15 @@ router.post("/getstore", (req, res) => {
     });
   }
 });
+
+function MasterStore_Check(id) {
+  return new Promise((resolve, reject) => {
+    let sql = "select  * from master_store where ms_id=?";
+    SelectParameter(sql, [id], (err, result) => {
+      if (err) reject(err);
+      console.log(result);
+
+      resolve(result);
+    });
+  });
+}
